8091973fcb767932dbaf5000d497b8155e85aa09
WeberPyth <shangnuo7015@126.com>
2019-11-11 07:33:26 +0800

    '2019.11.11'

--- Python/Stage_01/main/03算法分析/01级数/04冒泡排序.py
+++ Python/Stage_01/main/03算法分析/01级数/04冒泡排序.py
@@ -3,12 +3,14 @@
 冒泡排序法就是外层循环遍历整个数组，内层循环每次比对其中相邻的两个元素，将较大(或较小)的元素置于
 后置，该算法由于每次外层循环都会将一个元素放置到正确的位置上，故称冒泡排序法
 '''
-A = [1,3,2,5,4]
+A = [5,4,3,2,1]
+count = 0
 #原版
 def Solution_First(A):
-    
+    global count    
     for i in range( len(A) ):
         for j in range( len(A) - 1 ):
+            count += 1
             if A[j] > A[j + 1]:
                 mid = A[j]
                 A[j] = A[j+1]
@@ -18,15 +20,19 @@ def Solution_First(A):
 
 
 '''
-print( Solution_First(A) )
+#print( Solution_First(A) )
 '''
 
-#改进版1
-def Solution_Second(A):
+#改进版
+def Solution_Adv(A):
+    global count
     sign = True
+    n = len(A)
     while(sign):
         sign = False
-        for j in range( len(A) - 1 ):
+        n -= 1
+        for j in range( n ):
+            count += 1
             if A[j] > A[j + 1]:
                 mid = A[j]
                 A[j] = A[j+1]
@@ -38,6 +44,7 @@ def Solution_Second(A):
 '''        
 print( Solution_Second(A) )
 '''
+print(count)
 
 
 
